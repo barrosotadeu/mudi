858ef5e25fd6ccb5f0064ec7d6b4264fdf4c0860
fredyefra <fredweb3@gmail.com>
2022-12-13 21:54:46 -0300

    Update HomeController.java

--- eclipse-workspace/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ eclipse-workspace/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -2,34 +2,30 @@ package br.com.alura.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 
-
 @Controller
+@RequestMapping(value = "pages")
 public class HomeController {
-	
-	//private EntityManagerFactory emf = Persistence.createEntityManagerFactory("mudi");
-	@PersistenceContext
-	private EntityManager em;
-	
+
+	// private EntityManagerFactory emf =
+	// Persistence.createEntityManagerFactory("mudi");
+	@Autowired
+	private HomeService service;
+
 	@GetMapping("/home")
 	public String home(Model model) {
-		
-		
-		Query query = em.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();
-		
+
+		List<Pedido> pedidos = service.consultarPedidos();
+
 		model.addAttribute("pedidos", pedidos);
-		
+
 		return "home";
 	}
 }
